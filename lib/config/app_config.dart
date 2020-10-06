522d36be6b3c80ce9d7a7b2fe8c5cfe63f691958
Inex Code <gitea@inex.rocks>
2020-10-06 23:37:47 +0300

    More forking chores

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,9 +1,9 @@
 abstract class AppConfig {
-  static const String applicationName = 'FluffyChat';
+  static const String applicationName = 'FurryChat';
   static const String defaultHomeserver = 'matrix.tchncs.de';
   static const String privacyUrl = 'https://fluffychat.im/en/privacy.html';
   static const String sourceCodeUrl =
-      'https://gitlab.com/ChristianPauly/fluffychat-flutter';
+      'https://github.com/innereq/FurryChat';
   static const String supportUrl =
-      'https://gitlab.com/ChristianPauly/fluffychat-flutter/issues';
+      'https://github.com/innereq/FurryChat/issues';
 }
