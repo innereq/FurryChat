afba2bc1fe90ea1f44077cfd53e1e41374e56fd5
Inex Code <gitea@inex.rocks>
2020-10-06 22:15:40 +0000

    Fix formatting

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -2,8 +2,7 @@ abstract class AppConfig {
   static const String applicationName = 'FurryChat';
   static const String defaultHomeserver = 'matrix.tchncs.de';
   static const String privacyUrl = 'https://fluffychat.im/en/privacy.html';
-  static const String sourceCodeUrl =
-      'https://github.com/innereq/FurryChat';
+  static const String sourceCodeUrl = 'https://github.com/innereq/FurryChat';
   static const String supportUrl =
       'https://github.com/innereq/FurryChat/issues';
 }
